4f21b90afb1f4425cdb2f754079972465985ee0d
gitzhubj <143865050+gitzhubj@users.noreply.github.com>
2024-12-28 21:58:51 +0800

    Update setup.py

--- libsophon/setup.py
+++ libsophon/setup.py
@@ -2,7 +2,7 @@ import setuptools
 setuptools.setup(name='deeplearnt',
       version='0.0.2',
       description='clinical trial information retriver',
-      url='github项目地址 https://github.com/pergrand/deeplearntool',
+      url='github项目地址 https://github.com/gitzhubj/for_libsophon',
       author='User Image 239728614-4q_q.com ',
       author_email='邮箱',
       license='MIT',
