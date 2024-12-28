18f5be6c564c3081f6f1bb96d5285ec6715d5f92
gitzhubj <143865050+gitzhubj@users.noreply.github.com>
2024-12-28 21:56:10 +0800

    Add files via upload

---
+++ libsophon/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='deeplearnt',
+      version='0.0.2',
+      description='clinical trial information retriver',
+      url='github项目地址 https://github.com/pergrand/deeplearntool',
+      author='User Image 239728614-4q_q.com ',
+      author_email='邮箱',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
